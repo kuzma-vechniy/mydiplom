a5fcb3a8326d6733f3f19436688b6ae7d0c7ba2b
Anastasia <anastasiatea@mail.ru>
2019-05-03 17:00:33 +0300

    Стилизация таблицы корзины

--- pages/get/site/basket.php
+++ pages/get/site/basket.php
@@ -6,31 +6,37 @@
         <main class="wrap wrap__fixed">
         <article class="content ">
             <h2 class="content--title">Корзина</h2>
-            <table>
+            <table class="table">
                 <thead>
-                    <th>Ваш заказ</th>
-                    <th></th>
-                    <th>Количество</th>
-                    <th>Сумма</th>
+                <tr class="table--head">
+                    <td>Ваш заказ</td>
+                    <td></td>
+                    <td>Количество</td>
+                    <td>Сумма</td>
+                </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td> <img src="css/img/product.png"></td>
                         <td>
-                            <span>
+                        <div class="col col__left">
+                            <span class="table--text-primary">
                                 Ноутбук LENOVO IdeaPad 330-15IKBR
                             </span>
-                            <span>35400 ₽</span>
+                            <span class="table--text-bold">35400 ₽</span>
+                        </div>
                         </td>
                         <td>
-                            <input type="number">
-                            <a href="#">Удалить</a>
+                        <div class="col col__left">
+                            <input type="number" class="table--input-number">
+                            <a href="#" class="table--link">Удалить</a>
+                        </div>
                         </td>
                         <td>
                             <span>35400 ₽</span>
                         </td>
                     </tr>
-                    <tr>
+                    <tr class="table--head">
                         <td></td>
                         <td></td>
                         <td><span>Общая стоимость вашего заказа:</span></td>
@@ -38,6 +44,11 @@
                     </tr>
                 </tbody>
             </table>
+            <h2 class="content--title">Есть купон?</h2>
+            <form class="form">
+                <label class="form--label">Ввести код</label>
+                <input type="text" class="form--input">;
+            </form>
         </article>
         
         </main>
