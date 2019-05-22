20ebd6cd653b1ec8ec9a262b205aed3092ae58d0
Anastasia <anastasiatea@mail.ru>
2019-05-22 21:37:12 +0300

    стилистические правки футера

--- pages/get/site/help.php
+++ pages/get/site/help.php
@@ -3,11 +3,12 @@
     <? template('head', ['title' => $title]); ?>
     <body>
         <? template('header'); ?>
-
-<article class="help">
-    <h2 class="help--title">Как оформить заказ</h2>
-    <pre class="help--text">Когда вы определились с выбором, поместите товар в корзину, нажав «Купить» на его странице.</pre>
-</article>
+        <main class="wrap wrap__fixed">
+        <article class="help">
+            <h2 class="help--title">Как оформить заказ</h2>
+            <p class="help--text">Когда вы определились с выбором, поместите товар в корзину, нажав «Купить» на его странице.</p>
+        </article>
+</main>
         <? template('footer'); ?>
     </body>
 </html>
